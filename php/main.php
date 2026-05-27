1ff8e23b8bbb0429788d3914daf516ef7aacf8a6
Tsjippy <ewaldha@hotmail.com>
2026-05-27 12:29:43 +0200

    fixed: ?? bug

--- php/main.php
+++ php/main.php
@@ -29,7 +29,7 @@ add_filter('display_post_states', __NAMESPACE__.'\postStates', 10, 2);
  */
 function postStates( $states, $post ) {
 
-	if ( $post->ID == SETTINGS['all-contacts-page'] ?? false ) {
+	if ( $post->ID == (SETTINGS['all-contacts-page'] ?? false )) {
 		$states[] = __('Page showing all users');
 	}
 
