3572b93de7d1b4e28a151040c54542fcc40615b2
Tsjippy <ewaldha@hotmail.com>
2026-06-05 22:02:14 +0200

    added: shared functionality loader

--- tsjippy-user-pages.php
+++ tsjippy-user-pages.php
@@ -16,7 +16,7 @@ use TSJIPPY;
  * Plugin URI:            https://github.com/Tsjippy/userpages
  * Tested:                6.9
  * TextDomain:            tsjippy
- * Requires Plugins:    tsjippy-shared-functionality
+ * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
@@ -26,7 +26,10 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-
+// Load shared code
+if(file_exists(__DIR__  . '/shared_functionality/loader.php')){
+    require_once(__DIR__  . '/shared_functionality/loader.php');
+}
 
 // Define constants
 define(__NAMESPACE__ . '\PLUGIN', plugin_basename(__FILE__));
