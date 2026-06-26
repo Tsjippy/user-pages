a9ce5e910fe887ea10c175d44808ffabb3b4f34d
Tsjippy <ewaldha@hotmail.com>
2026-06-26 12:40:29 +0200

    changed: plugin tested up to 7.0

--- tsjippy-user-pages.php
+++ tsjippy-user-pages.php
@@ -12,7 +12,7 @@ use TSJIPPY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         6.9
+ * Tested up to:         7.0
  * Plugin URI:            https://github.com/Tsjippy/userpages
  * Tested:                6.9
  * TextDomain:            tsjippy
