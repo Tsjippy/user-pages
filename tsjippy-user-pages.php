75614d9d18d17cac72f6b8c3a0e52a14571ea67c
Tsjippy <ewaldha@hotmail.com>
2026-07-03 18:59:40 +0200

    fixed: plugin settings key

--- tsjippy-user-pages.php
+++ tsjippy-user-pages.php
@@ -36,7 +36,7 @@ define(__NAMESPACE__ . '\PLUGIN', plugin_basename(__FILE__));
 define(__NAMESPACE__ . '\PLUGINPATH', __DIR__ . '/');
 define(__NAMESPACE__ . '\PLUGINVERSION', get_plugin_data(__FILE__, false, false)['Version']);
 define(__NAMESPACE__ . '\PLUGINSLUG', str_replace('tsjippy-', '', basename(__FILE__, '.php')));
-define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_' . PLUGINSLUG . '_settings', []));
+define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_user-pages_settings', []));
 
 // run right before activation
 register_activation_hook(__FILE__, function () {
@@ -50,7 +50,7 @@ register_activation_hook(__FILE__, function () {
 
     $settings['all-contacts-page']    = TSJIPPY\ADMIN\createDefaultPage('All Users', '[tsjippy_all_contacts]');
 
-    update_option('tsjippy_' . PLUGINSLUG . '_settings', $settings);
+    update_option('tsjippy_user-pages_settings', $settings);
 
     if(function_exists('TSJIPPY\activate')){
         \TSJIPPY\activate();
