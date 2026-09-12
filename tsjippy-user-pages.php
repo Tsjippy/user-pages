74a45de76107078ace5a3bb6d08b667f4a20ddde
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:21 +0200

    changed: removed 'tested up to'

--- tsjippy-user-pages.php
+++ tsjippy-user-pages.php
@@ -12,10 +12,9 @@ use TSJIPPY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
- * Plugin URI:            https://github.com/Tsjippy/userpages
+ * Plugin URI:           https://github.com/Tsjippy/userpages
  * Tested:               7.1
- * TextDomain:            tsjippy
+ * TextDomain:           tsjippy
  * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
